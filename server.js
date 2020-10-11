fb42cade10216ed1c3512ada262eea2085fdd1b2
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-11 23:59:11 +0800

    Test CD

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port);
