5272de1ebee6ac108e0192886434f58c1ea829d3
jon-chua <jonchua97@gmail.com>
2020-10-12 13:36:52 +0800

    Style signin and register pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
 
 import logo from "../images/logo-white.png";
+import ROUTES from "../routes/Routes";
 
-const Nav = () => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand>
-      <img
-        alt=""
-        src={logo}
-        width="40"
-        height="40"
-        className="d-inline-block align-top mr-3"
-      />
-      YogaPets
-    </Navbar.Brand>
-  </Navbar>
-);
+const Nav = () => {
+  const history = useHistory();
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand onClick={() => history.push(ROUTES.HOME)}>
+        <img
+          alt=""
+          src={logo}
+          width="40"
+          height="40"
+          className="d-inline-block align-top mr-3"
+        />
+        YogaPets
+      </Navbar.Brand>
+    </Navbar>
+  );
+}
 
 export default Nav;
