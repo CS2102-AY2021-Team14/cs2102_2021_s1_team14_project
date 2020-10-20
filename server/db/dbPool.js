1437e6b4556401e397b87227a0e7e8379c8810c8
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-20 12:34:55 +0800

    test heroku endpoint

--- server/db/dbPool.js
+++ server/db/dbPool.js
@@ -1,22 +1,22 @@
 const { Pool } = require("pg");
 
 // Production
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.DB_USESSL
-//     ? {
-//         rejectUnauthorized: false,
-//       }
-//     : null,
-// });
-
-// DEV ENV (Clarence)
 const pool = new Pool({
-  user: "postgres",
-  password: "zxcv",
-  host: "localhost",
-  port: 5432,
-  database: "yogapets"
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_USESSL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : null,
 });
+
+// // DEV ENV (Clarence)
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "zxcv",
+//   host: "localhost",
+//   port: 5432,
+//   database: "yogapets"
+// });
 
 module.exports = pool;
