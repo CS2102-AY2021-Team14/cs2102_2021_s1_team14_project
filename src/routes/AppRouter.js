05324536f55186974a536995dfc353ddf25bf544
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-24 13:41:18 +0800

    Add context to app router

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import PublicRoute from "./PublicRoute";
 import PrivatePetOwnerRoute from "./PrivatePetOwnerRoute";
@@ -11,18 +11,22 @@ import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import Register from "../pages/Register";
 
+import { UserContext } from "../utils/UserProvider";
+
 import PetOwnerHome from "../pages/petowner/PetOwnerHome";
 
+const AppRouter = () => {
+  const { username, authToken, roles } = useContext(UserContext);
+  // ^ use these to determine whether logged in or not and what role (roles is an array)
+  // roles can be an array containing these stuff ["Pet Owner", "Full-time Care Taker", "Part-time Care Taker", "PCS Admin"]
 
-
-const AppRouter = () =>{ 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const setAuth = (boolean) => {
+  const setAuth = boolean => {
     setIsAuthenticated(boolean);
   };
-  
-  return(
+
+  return (
     <Fragment>
       <ToastContainer />
       <BrowserRouter>
@@ -32,17 +36,41 @@ const AppRouter = () =>{
 
           <PrivatePetOwnerRoute path={ROUTES.PET_OWNER_HOME} component={PetOwnerHome} />
           <PublicRoute path={ROUTES.HOME} exact={true} component={Home} /> */}
-          <Route path={ROUTES.REGISTER} render={ props => !isAuthenticated ? <Register {...props} setAuth={setAuth} /> 
-                                                                            : (<Redirect to={ROUTES.SIGN_IN} />) } />
-          <Route path={ROUTES.SIGN_IN} render={ props => !isAuthenticated ? <SignIn {...props} setAuth={setAuth} />
-                                                                          : (<Redirect to={ROUTES.PET_OWNER_HOME} />) } />
-          <Route path={ROUTES.PET_OWNER_HOME} render={ props => isAuthenticated ? (<PetOwnerHome {...props} />) 
-                                                                                : (<Redirect to={ROUTES.SIGN_IN} />) } />
+          <Route
+            path={ROUTES.REGISTER}
+            render={props =>
+              !isAuthenticated ? (
+                <Register {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to={ROUTES.SIGN_IN} />
+              )
+            }
+          />
+          <Route
+            path={ROUTES.SIGN_IN}
+            render={props =>
+              !isAuthenticated ? (
+                <SignIn {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to={ROUTES.PET_OWNER_HOME} />
+              )
+            }
+          />
+          <Route
+            path={ROUTES.PET_OWNER_HOME}
+            render={props =>
+              isAuthenticated ? (
+                <PetOwnerHome {...props} />
+              ) : (
+                <Redirect to={ROUTES.SIGN_IN} />
+              )
+            }
+          />
           <PublicRoute path={ROUTES.HOME} exact={true} component={Home} />
         </Switch>
       </BrowserRouter>
     </Fragment>
   );
-}
+};
 
 export default AppRouter;
