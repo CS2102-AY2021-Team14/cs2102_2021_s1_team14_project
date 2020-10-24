69346316efafe0a90c2479f78a46bd9f6aea5a53
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-10-24 11:36:38 +0800

    Fix db pool ssl

--- server/db/dbPool.js
+++ server/db/dbPool.js
@@ -3,12 +3,12 @@ const { Pool } = require("pg");
 // // Production
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_USESSL
-    ? {
-        rejectUnauthorized: false,
-      }
-    : null,
-  sslmode: process.env.NODE_ENV === "production" ? "require" : "disable"
+  ssl:
+    process.env.DB_USESSL === "true"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : null,
 });
 
 // DEV ENV (Clarence)
