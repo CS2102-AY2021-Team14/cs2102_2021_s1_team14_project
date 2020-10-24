f128e1ee15e4355c85a325c045c23a71f92b64a0
jon-chua <jonchua97@gmail.com>
2020-10-24 11:51:01 +0800

    Use axios

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Container, Form, Row, Col } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 import Navbar from "../components/Navbar";
 import ROUTES from "../routes/Routes";
@@ -15,29 +16,21 @@ const SignIn = ({ setAuth }) => {
   const history = useHistory();
 
   const handleSubmit = async (event) => {
-    try {
       event.preventDefault();
       console.log(`Form submitted, Username: ${username}, Password: ${password}`);
 
       const body = { username, password };
-      const response = await fetch("/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify(body)
-      });
-      const parseRes = await response.json();
 
-      if (parseRes.token) {
-        localStorage.setItem("token", parseRes.token);
-        setAuth(true);
-        toast.success("Login Successfully!");
-      } else {
-        setAuth(false);
-        toast.error(parseRes);
-      }
-    } catch(error) {
-      console.error(error.message);
-    }
+      axios.post("/api/auth/login", body).then(response => {
+
+        const token = JSON.parse(response.data).token;
+        // setCurrentAuth(token);
+        
+        // Decode token and check for petowner/caretaker/admin
+
+      }).catch(error => {
+        toast.error(error);
+      });
   }
 
   return (
