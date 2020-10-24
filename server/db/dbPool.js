f6b66de4ddd7b71178a073aebcce2055ab99b6f2
jon-chua <jonchua97@gmail.com>
2020-10-24 11:25:47 +0800

    Add db ssl config

--- server/db/dbPool.js
+++ server/db/dbPool.js
@@ -1,22 +1,23 @@
 const { Pool } = require("pg");
 
 // // Production
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.DB_USESSL
-//     ? {
-//         rejectUnauthorized: false,
-//       }
-//     : null,
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_USESSL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : null,
+  sslmode: process.env.NODE_ENV === "production" ? "require" : "disable"
+});
 
 // DEV ENV (Clarence)
-const pool = new Pool({
-  user: "postgres",
-  password: "zxcv",
-  host: "localhost",
-  port: 5432,
-  database: "yogapets"
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "postgres",
+//   host: "localhost",
+//   port: 8080,
+//   database: "yogapets"
+// });
 
 module.exports = pool;
