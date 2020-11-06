e6d34f1b44974cf7fa84d05538ffb6804fba9375
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-11-06 21:00:31 +0800

    Minor UI updates

--- src/pages/petowner/PetOwnerHome.js
+++ src/pages/petowner/PetOwnerHome.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 import PetOwnerSidebar from "../../components/sidebar/PetOwnerSidebar";
 import Navbar from "../../components/Navbar";
 import BidCard from "../../components/BidCard";
+import { UserContext } from "../../utils/UserProvider";
+import axios from "axios";
 
 import { RiAuctionFill } from "react-icons/ri";
 
@@ -39,6 +41,70 @@ export const MOCK_DATA = [
 ];
 
 const PetOwnerHome = () => {
+  const { username } = useContext(UserContext);
+
+  const [bids, setBids] = useState([]);
+
+  const fetchBids = () => {
+    axios
+      .get(`/api/bids/owner/${username}`)
+      .then(res => {
+        const data = res.data?.data;
+
+        const fetchedBids = data?.map(bidObj => {
+          const {
+            care_taker,
+            care_taker_name,
+            pet,
+            pet_type,
+            start_date,
+            end_date,
+            is_successful,
+            is_active,
+            payment_type,
+            transfer_method,
+            rating,
+            review_text,
+          } = bidObj;
+
+          return {
+            caretakerUsername: care_taker,
+            caretakerName: care_taker_name,
+            pet,
+            petType: pet_type,
+            startDate: start_date,
+            endDate: end_date,
+            isSuccessful: is_successful,
+            isActive: is_active,
+            paymentType: payment_type,
+            transferMethod: transfer_method,
+            rating,
+            reviewText: review_text,
+          };
+        });
+
+        setBids(
+          fetchedBids.sort((a, b) => {
+            const dateDiff =
+              new Date(a.startDate).setHours(0, 0, 0, 0) -
+              new Date(b.startDate).setHours(0, 0, 0, 0);
+            if (dateDiff !== 0) {
+              return dateDiff;
+            } else {
+              return a.caretakerUsername - b.caretakerUsername;
+            }
+          }) ?? []
+        );
+      })
+      .catch(err => console.log("Error fetching pet owner's bids", err));
+  };
+
+  useEffect(() => {
+    if (username) {
+      fetchBids();
+    }
+  }, [username]);
+
   return (
     <div>
       <Navbar />
@@ -54,9 +120,13 @@ const PetOwnerHome = () => {
                   <RiAuctionFill />
                 </span>
                 Bids Overview
+                <br />
+                (Up to 5 Upcoming Bids,
+                <br />
+                visit the Bids tab to view yours bids)
               </Card.Header>
               <Card.Body>
-                {MOCK_DATA.map((data, index) => (
+                {bids.slice(0, Math.min(bids.length, 5)).map((data, index) => (
                   <BidCard {...data} key={index} />
                 ))}
               </Card.Body>
