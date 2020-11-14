c93fcec581c9ee484bb572b87244fa6b3b7a5318
fatclarence <ckoh.clarence.96@gmail.com>
2020-11-14 17:53:37 +0800

    add structure for salary homepage for caretaker

--- src/components/salary/Salary.js
+++ src/components/salary/Salary.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { 
+    Table,
+    TableRow,
+    TableCell,
+    TableHead,
+    TableBody,
+  } from '@material-ui/core';
 import './Salary.css';
 
-const Salary = ({jobs}) => {
-
+const Salary = ({ allSalaries }) => {
+    
     return (
-        <div>page content</div>
+        <>
+           { allSalaries ? 
+            <Table>
+                <TableHead>
+                    <TableCell>Month</TableCell>
+                    <TableCell>Year</TableCell>
+                    <TableCell>Pet Days</TableCell>
+                    <TableCell>Salary</TableCell>
+                </TableHead>
+                <TableBody>
+                    {allSalaries.map(row => (
+                        <TableRow>
+                            <TableCell>{row.month}</TableCell>
+                            <TableCell>{row.year}</TableCell>
+                            <TableCell>{row.pet_days}</TableCell>
+                            <TableCell>{row.amount}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table> : null }
+        </>
     )
 
 }
--- src/pages/caretaker/CareTakerSalary.js
+++ src/pages/caretaker/CareTakerSalary.js
@@ -4,24 +4,33 @@ import Navbar from '../../components/Navbar';
 import CaretakerSidebar from '../../components/sidebar/CaretakerSidebar';
 import axios from 'axios';
 import { UserContext } from "../../utils/UserProvider";
-import YogaPetsLogo from '../../images/logo.png';
-import Avatar from '../../components/avatar/Avatar';
+// import YogaPetsLogo from '../../images/logo.png';
+// import Avatar from '../../components/avatar/Avatar';
 import Salary from '../../components/salary/Salary';
-
-
-
+import { Typography } from "@material-ui/core";
 
 const CareTakerSalary = () => {
   
   // Caretaker information
   const { username } = useContext(UserContext); 
+  const [allSalaries, setAllSalaries] = useState([])
 
-  
+  const getCareTakerSalary = (username) => {
+    axios.get(`/api/caretaker/${username}/salary`)
+          .then(res => {
+            const { data } = res;
+            const fetchedSalaries = data.data ?? [];
+            console.log(fetchedSalaries);
+            setAllSalaries(fetchedSalaries);
+          })
+          .catch(err => console.error(err.response.data.message));
+  }
+
   useEffect(() => {
     if (username) {
       getCareTakerSalary(username);
     }
-  }, [username])
+  }, [username]);
   // Find employment
   // const findEmployment = () => {
   //     if (caretakerJobs.length < 1) {
@@ -50,7 +59,13 @@ const CareTakerSalary = () => {
               <CaretakerSidebar defaultKey={"Salary"} />
             </Col>
             <Col xs={8} id="page-content">
-              <Salary jobs={caretakerInfo.jobs} />
+              <Typography variant='h6'>
+                You've been working hard {username} !
+              </Typography>
+              <Typography variant='h6'>
+                Here are your monthly salaries:
+              </Typography>
+              {allSalaries && <Salary allSalaries={allSalaries} />}
             </Col>
             {/* <Col xs={2} id="avatar">
               <Avatar user={caretakerInfo} />
